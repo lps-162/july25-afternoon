14b84391c4a2c478f91b2e9acde794b19de87a2d
LP Shivan <lp.shivan@gmail.com>
2017-07-25 14:47:25 +0530

    UST Global July Last week - Afternoon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LearnComponent } from './learn/learn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    LearnComponent
   ],
   imports: [
     BrowserModule
